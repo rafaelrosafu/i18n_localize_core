db91aba3ea5072fdc4c10d53bfd837545d6eefaf
Rafael Rosa Fu <rafaelrosafu@gmail.com>
2009-02-11 04:21:47 +0100

    Initial commit to i18n_localize_core.

---
+++ test/test_helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+require 'mocha'
+
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'i18n_localize_core'
+
+class Test::Unit::TestCase
+end
