b8662965809521c5aafcb0b174e3900f272b9b41
Rafael Rosa Fu <rafaelrosafu@gmail.com>
2009-08-23 02:18:15 -0300

    Covering frozen string bug with a regression test

--- test/string_class_test.rb
+++ test/string_class_test.rb
@@ -4,10 +4,15 @@ class StringClassTest < Test::Unit::TestCase
   def setup
     setup_I18n
   end
-  
+
   def test_as_delocalized_number
     assert_equal @delocalized_number, @localized_number_en.as_delocalized_number
     I18n.locale = :'pt-br'
     assert_equal @delocalized_number, @localized_number_pt_br.as_delocalized_number
   end
+
+  def test_bug_delocalized_number_with_frozen_string
+    assert_equal @delocalized_number, @localized_number_en.freeze.as_delocalized_number
+  end
 end
+
