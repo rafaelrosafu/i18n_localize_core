c43df115a65a0bf9a970491738c2b92733aa7be0
Rafael Rosa Fu <rafaelrosafu@gmail.com>
2009-08-24 17:00:22 -0300

    Fixed the way the original parse method is called

--- lib/i18n_localize_core/date_extensions.rb
+++ lib/i18n_localize_core/date_extensions.rb
@@ -3,9 +3,9 @@ class Date
     alias original_parse _parse
     def _parse(str, comp=false)
       #use original_parse only if the date is in yyyy-mm-dd format
-      if str.match /\d{4}-\d{2}-\d{2}/
+      if (!I18n.localize_core) || str.match(/\d{4}-\d{2}-\d{2}/)
         original_parse(str, comp)
-      elsif I18n.localize_core
+      else
         p = I18n.parse_date str
         #returns a hash with year, month and day, as original parse does
         {:year => p.year, :mday => p.mday, :mon => p.mon} unless p.nil?
--- lib/i18n_localize_core/i18n_extensions.rb
+++ lib/i18n_localize_core/i18n_extensions.rb
@@ -6,54 +6,54 @@ module I18n
         class << self
           alias_method :original_reload, :'reload!'
           alias_method :original_locale, :locale=
-          
+
           def reload!
             initialize_separators
             original_reload
           end
-          
+
           def locale=(value)
             initialize_separators
             original_locale(value)
           end
         end
       end
-      
+
       base.extend ClassMethods
     end
-    
+
     module ClassMethods
       def parse_date(target)
         raise ::ArgumentError unless target.is_a? String
-        
-        if target.length <= 10
-          _parse_date(target, [:date, :formats, :default])
+        clean_target = target.strip
+        if clean_target.length <= 10
+          _parse_date(clean_target, [:date, :formats, :default])
         else
-          _parse_date(target, [:time, :formats, :long])
+          _parse_date(clean_target, [:time, :formats, :long])
         end
       end
-      
+
       def parse_number(target)
         raise ::ArgumentError unless target.is_a? String
         target.to_f true
       end
-      
+
       def localize_core
         @@localize_core ||= false
       end
-      
+
       def localize_core=(value)
         @@localize_core = value
       end
-      
+
       def thousand_separator
         @@thousand_separator ||= I18n.translate(:'number.format.delimiter', :raise => true) rescue nil
       end
-      
+
       def decimal_separator
         @@decimal_separator ||= I18n.translate(:'number.format.separator', :raise => true) rescue nil
       end
-      
+
       # searches for a translation key in the I18n backend
       def lookup_translation(*keys)
         keys.flatten!
@@ -69,13 +69,13 @@ module I18n
           end
         end
       end
-      
+
       protected
         def initialize_separators
           @@thousand_separator = nil
           @@decimal_separator = nil
         end
-        
+
         def _parse_date(target, format_key)
           # search for the format
           format = I18n.lookup_translation(I18n.locale, format_key)
